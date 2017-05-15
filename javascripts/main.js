5a88f06ee7f8edc5824293a0dc10c0eb2e6d8a45
sharon smith <sharon.e.smith2@gmail.com>
2017-05-14 23:06:40 -0500

    writing addresses to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,15 +9,14 @@ app.controller("AddressCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
 
 	let getFBAddresses = () => {
-// console.log("in getFBAddresses");
+		
 		let addrezzez = [];
 
 		return $q((resolve, reject) => {
-			$http.get(`$(FIREBASE_CONFIG.databaseURL}/addresses.json`)
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/addresses.json`)
 			.then((fbAddresses) => {
-console.log("fbAddresses :: ", fbAddresses);
 				let fbAddressList = fbAddresses.data;
-				Object.key(fbAddressList).forEach((key) => {
+				Object.keys(fbAddressList).forEach((key) => {
 					fbAddressList[key].id = key;
 					addrezzez.push(fbAddressList[key]);
 				});
