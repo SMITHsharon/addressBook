b394b73f2bac3eaa74d91180d9c23a20e23ed173
sharon smith <sharon.e.smith2@gmail.com>
2017-05-14 22:51:15 -0500

    AppConstants, code structure for index.html, main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,47 @@
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
 
-var app = angular.module("addressBook", []);
 
-app.controller("ItemCtrl", ($scope) => {
-	$scope.itemText = "writing this variable to the DOM;)";
+app.controller("AddressCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+	
+	$scope.addresses = [];
+
+
+	let getFBAddresses = () => {
+// console.log("in getFBAddresses");
+		let addrezzez = [];
+
+		return $q((resolve, reject) => {
+			$http.get(`$(FIREBASE_CONFIG.databaseURL}/addresses.json`)
+			.then((fbAddresses) => {
+console.log("fbAddresses :: ", fbAddresses);
+				let fbAddressList = fbAddresses.data;
+				Object.key(fbAddressList).forEach((key) => {
+					fbAddressList[key].id = key;
+					addrezzez.push(fbAddressList[key]);
+				});
+					resolve(addrezzez);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+
+	let getAddresses = () => {
+
+		getFBAddresses()
+		.then((fbAddresses) => {
+			$scope.addresses = fbAddresses;
+		})
+		.catch((error) => {
+			console.log("error in getAddresses", error);
+		});
+	};
+
+	getAddresses();
+
 });
 
